0d844c843aea60c31afd03f96d27b740c241dfc9
Marc Langenbach <mlangen@absint.com>
2016-12-22 13:08:55 +0100

    bug 20380 removed not needed files

--- abstractinfodock.cpp
+++ abstractinfodock.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008, Pino Toscano <pino@kde.org>
+ * Copyright (C) 2016, Marc Langenbach <mlangen@absint.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "fonts.h"
+#include "abstractinfodock.h"
 
 AbstractInfoDock::AbstractInfoDock(QWidget *parent)
     : QDockWidget(parent), m_filled(false)
